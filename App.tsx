76f07a25923b794a0af1f8c424a6b11c13d573c0
woyong <woyongegbara@yahoo.com>
2019-12-21 19:42:09 +0100

    Initial push

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => (
+  <View style={{justifyContent: 'center', flex: 1, alignItems: 'center'}}>
+    <Text>UP TOWN</Text>
+  </View>
+);
+
+export default App;
